90d1a4d1eba6ffa61e28d5b179471bd9e8e2327f
Arsenii Dumitru <arsdu24@gmail.com>
2021-03-12 16:15:12 +0200

    wip(*): first patch

--- packages/core/src/nodes/index.ts
+++
@@ -1,4 +0,0 @@
-import './register.node';
-
-export * from './native-factory.node';
-export * from './reflect-props-metadata.node';
--- packages/core/src/rx/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './combine-assoc.rx';
-export * from './hot.rx';
-export * from './listen-needs.rx';
